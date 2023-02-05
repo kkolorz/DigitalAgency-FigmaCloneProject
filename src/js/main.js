37cc284fdaffff81c5a4d06501624ba22c3effcc
kkolorz <61511735+kkolorz@users.noreply.github.com>
2023-02-05 22:20:34 +0100

    Added contact popup

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,9 @@ const navMobile = document.querySelector('.nav__items');
 const allNavItems = document.querySelectorAll('.nav__item');
 const showNavBtn = document.querySelector('.nav__btn');
 const closeNavBtn = document.querySelector('.close-btn');
+const popup = document.querySelector('.popup');
+const closePopup = document.querySelector('.popup__window--btn');
+const showPopup = document.querySelector('.contact--btn');
 
 
 const handleNav = () => {
@@ -13,7 +16,12 @@ const handleNav = () => {
         })
     })
 }
-console.log(window.scrollY);
+
+const handlePopup = () => {
+    popup.classList.toggle('popup__close');
+}
 
 showNavBtn.addEventListener('click', handleNav);
 closeNavBtn.addEventListener('click', handleNav);
+closePopup.addEventListener('click', handlePopup);
+showPopup.addEventListener('click', handlePopup);
